93ada8ed282c2a296a08cbd324c196164a6b3ed5
shivy108 <shivy101@live.com>
2020-11-30 19:36:52 +0100

    stars

--- src/App.js
+++ src/App.js
@@ -1,56 +1,75 @@
+import { Box, Circle, Extrude, OrbitControls, RoundedBox, Stars } from "drei";
 import { Canvas, useFrame, useThree } from "react-three-fiber";
 import { Physics, useSphere, useBox, usePlane } from "use-cannon";
+import "./App.scss";
 
-const Ball = ({ args = [1, 32, 32] }) => {
-  const { viewport } = useThree();
-  const [ref, api] = useSphere(() => ({ args: 0.5, mass: 1 }));
+// const Ball = ({ args = [1, 1, 1] }) => {
+//   const { viewport } = useThree();
+//   const [ref, api] = useSphere(() => ({ args: 0.5, mass: 1 }));
 
-  usePlane(() => ({
-    position: [0, viewport.height, 0],
-    rotation: [-Math.PI / 2, 0, 0],
-    onCollide: () => {
-      api.position.set(0, 0, 0);
-      api.velocity.set(0, 10, 0);
-    },
-  }));
+//   usePlane(() => ({
+//     position: [0, viewport.height/2, 0],
+//     rotation: [-Math.PI / 2, 0, 0],
+//     onCollide: () => {
+//       api.position.set(0, 0, 0);
+//       api.velocity.set(0, 10, 0);
+//     },
+//   }));
 
+//   return (
+//     <mesh ref={ref}>
+//       <sphereBufferGeometry args={args} />
+//       <meshStandardMaterial color="pink" />
+//     </mesh>
+//   );
+// };
+
+// const Paddle = ({ args = [1, 0, 1] }) => {
+//   const [ref, api] = useBox(() => ({ args }));
+
+//   useFrame((state) => {
+//     api.position.set(
+//       (state.mouse.x * state.viewport.width) / 2,
+//       -state.viewport.height / 2,
+//       0
+//     );
+//     api.rotation.set(0, 0, (state.mouse.x * Math.PI) / 5);
+//   });
+//   return (
+//     <mesh ref={ref}>
+//       <boxBufferGeometry args={args} />
+//       <meshStandardMaterial color="white" />
+//     </mesh>
+//   );
+// };
+const Ball = () => {
   return (
-    <mesh ref={ref}>
-      <sphereBufferGeometry args={args} />
-      <meshStandardMaterial color='pink' />
+    <mesh>
+      <sphereBufferGeometry attach="geometry" />
+      <meshStandardMaterial attach="material" color="pink" />
     </mesh>
   );
 };
 
-const Paddle = ({ args = [2, 0.5, 1] }) => {
-  const [ref, api] = useBox(() => ({ args }));
-
-  useFrame((state) => {
-    api.position.set(
-      (state.mouse.x * state.viewport.width) / 2,
-      -state.viewport.height / 2,
-      0
-    );
-  });
+const Triangle = () => {
   return (
-    <mesh ref={ref}>
-      <boxBufferGeometry args={args} />
-      <meshStandardMaterial color='white' />
+    <mesh>
+      <circleBufferGeometry attach="geometry" args={[3, 3]} />
+      <meshStandardMaterial attach="material" color="pink" />
     </mesh>
   );
 };
 
 function App() {
   return (
-    <Canvas camera={{ positon: [0, 5, 12], fov: 50 }}>
+    <Canvas colorManagement>
       <ambientLight intensity={0.3} />
       <pointLight position={[10, 10, 5]} />
       <pointLight position={[-10, -10, -5]} />
-      <Physics
-        gravity={[0, -30, 0]}
-        defaultContactMaterial={{ restitution: 1.1 }}>
-        <Ball />
-        <Paddle />
+      <OrbitControls />
+      <Stars />
+      <Physics>
+        <Triangle/>
       </Physics>
     </Canvas>
   );
